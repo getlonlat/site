95e45d38a8703be978fb291569a0bb21efda9fb7
Éverton Inocêncio <hewerthomn@gmail.com>
2019-04-17 13:06:04 -0400

    Committed some changes

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+echo 'getlonlat.top' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:getlonlat/site.git master:gh-pages
+
+cd -
