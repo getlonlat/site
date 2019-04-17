87a4b67e821eb61a7c6e1a1c0ab1dc71a49cd658
Éverton Inocêncio <hewerthomn@gmail.com>
2019-04-17 15:33:46 -0400

    More ignore

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <section class="hero is-light is-medium">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            About
+          </h1>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
