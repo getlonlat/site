cf9841854fca23d7e3723b984be072e8cf9a0cf5
Éverton Inocêncio <hewerthomn@gmail.com>
2020-04-29 21:32:08 -0400

    deploy

---
+++ precache-manifest.3b63d3074f97bbb07fd9aa7107b4af23.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "62115d8427013931e77b",
+    "url": "/site/js/about.9c8c430a.js"
+  },
+  {
+    "revision": "96c653ac478ebf67d338",
+    "url": "/site/css/app.41cd21e7.css"
+  },
+  {
+    "revision": "96c653ac478ebf67d338",
+    "url": "/site/js/app.cc1418d6.js"
+  },
+  {
+    "revision": "f15f50c0d90a7243a14e",
+    "url": "/site/js/chunk-vendors.c2f8bd7a.js"
+  },
+  {
+    "revision": "3064bc685bb5656052ad3864a0bd0b54",
+    "url": "/site/img/target.3064bc68.png"
+  },
+  {
+    "revision": "532b27dca05f84434ebad09059edec9f",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "02d8b0891c8ea82087afccec4c532875",
+    "url": "/site/img/angular.png"
+  },
+  {
+    "revision": "e0eab74d48c6d753ef9bc506d665f376",
+    "url": "/site/img/made-with-bulma.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/site/robots.txt"
+  },
+  {
+    "revision": "27c9a479811ee68638baa924dd22aad8",
+    "url": "/site/img/vuejs.png"
+  },
+  {
+    "revision": "27863e70bf1b1838e4ffe9b7bbe4b2b6",
+    "url": "/site/img/angularjs.png"
+  }
+];
